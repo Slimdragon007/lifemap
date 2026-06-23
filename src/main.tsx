2e2db33d3dfd98c3feeab9135c1ef5b12a01a8a1
slimcode007 <m.haslim@gmail.com>
2026-06-22 23:19:08 -0700

    feat(theme): apply theme pre-paint (no flash) + on startup

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles.css";
 import "./touch-polish.css";
+import { applyTheme, getInitialTheme } from "./theme";
+applyTheme(getInitialTheme());
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
